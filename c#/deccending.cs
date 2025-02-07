5940616403531de9d102d9b99129b6b3dd7ddf0a
AkshaSarvaiya07 <akshasarvaiya@gmail.com>
2025-02-07 19:51:43 +0530

    Update deccending.cs

--- c#/deccending.cs
+++ c#/deccending.cs
@@ -3,7 +3,10 @@ class my
 {
     public static void Main()
     {
-        int a=10;
+        int a;
+        Console.Write("enter a value:");
+        a=Convert.ToInt32(Console.ReadLine());
+        
         while(a>=1)
         {
             Console.Write(a +"\n");
