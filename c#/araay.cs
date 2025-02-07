d7c985c8c57353a1f8e42b0eb12cb5580fbaacc6
AkshaSarvaiya07 <akshasarvaiya@gmail.com>
2025-02-06 21:09:26 -0800

    Add files via upload

---
+++ c#/araay.cs
@@ -0,0 +1,13 @@
+using System;
+class araay
+{
+    public static void Main()
+    {
+        int[] a={14,21,45};
+        for(int i=0;i<3;i++)
+        {
+            Console.WriteLine("a {0}:{1}", i+1 ,a[i]);
+        }
+    }
+
+}
