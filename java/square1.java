0d83f3ccd2d6f50dcc40248551ff5c1928090321
AkshaSarvaiya07 <akshasarvaiya@gmail.com>
2025-02-17 07:49:36 -0800

    Add files via upload

---
+++ java/square1.java
@@ -0,0 +1,13 @@
+public class square1
+{
+	public static void main(String[] args)
+	{
+		int sum=0;
+		for(int a=1;a<=10;a++)
+		{
+			sum+=a*a;
+			
+		}
+		System.out.println("sum of squre is:" +sum);
+	}
+}
