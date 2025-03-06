d16e1e4447af6bc059a68e57e7c5acd4456b6aa6
AkshaSarvaiya07 <akshasarvaiya@gmail.com>
2025-03-05 17:45:15 -0800

    Add files via upload

---
+++ c#/sealedmethod.cs
@@ -0,0 +1,21 @@
+public class parent
+
+ {
+		
+     public void method() 
+	{
+			
+	}
+
+}
+
+	public class child :parent
+
+    {
+
+        public sealed override void Method()
+		{
+				
+		}
+
+    }
